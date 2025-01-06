bad42072f53ca0e6bfbd0d334860fee629c218d1
Dmytro Hutnyk <s28785@pjwstk.edu.pl>
2025-01-06 03:05:40 +0100

    feat: add dto objects

--- src/main/java/hutnyk/notes_app/Model/DTO/NoteDTO.java
+++ src/main/java/hutnyk/notes_app/Model/DTO/NoteDTO.java
@@ -1,4 +1,64 @@
 package hutnyk.notes_app.Model.DTO;
 
+import java.util.Date;
+import java.util.Set;
+
 public class NoteDTO {
+
+    private String title;
+    private Date finishDate;
+    private Long userId;
+    private Set<Long> statusSetId;
+
+    public NoteDTO() {
+    }
+
+    public NoteDTO(String title, Date finishDate, Long userId, Set<Long> statusSetId) {
+        this.title = title;
+        this.finishDate = finishDate;
+        this.userId = userId;
+        this.statusSetId = statusSetId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getFinishDate() {
+        return finishDate;
+    }
+
+    public void setFinishDate(Date finishDate) {
+        this.finishDate = finishDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Set<Long> getStatusSetId() {
+        return statusSetId;
+    }
+
+    public void setStatusSetId(Set<Long> statusSetId) {
+        this.statusSetId = statusSetId;
+    }
+
+    @Override
+    public String toString() {
+        return "NoteDTO{" +
+                "title='" + title + '\'' +
+                ", finishDate=" + finishDate +
+                ", userId=" + userId +
+                ", statusSetId=" + statusSetId +
+                '}';
+    }
 }
--- src/main/java/hutnyk/notes_app/Model/DTO/UserDTO.java
+++ src/main/java/hutnyk/notes_app/Model/DTO/UserDTO.java
@@ -1,4 +1,63 @@
 package hutnyk.notes_app.Model.DTO;
 
+import java.util.Set;
+
 public class UserDTO {
+
+    private String username;
+    private String email;
+    private Set<Long> notesId;
+    private Set<Long> roleSetId;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(String username, String email, Set<Long> notesId, Set<Long> roleSetId) {
+        this.username = username;
+        this.email = email;
+        this.notesId = notesId;
+        this.roleSetId = roleSetId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Long> getNotesId() {
+        return notesId;
+    }
+
+    public void setNotesId(Set<Long> notesId) {
+        this.notesId = notesId;
+    }
+
+    public Set<Long> getRoleSetId() {
+        return roleSetId;
+    }
+
+    public void setRoleSetId(Set<Long> roleSetId) {
+        this.roleSetId = roleSetId;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", notesId=" + notesId +
+                ", roleSetId=" + roleSetId +
+                '}';
+    }
 }
