11f0a7b0d5bc37cd1090865b123f7212759f5caf
Dmytro Hutnyk <s28785@pjwstk.edu.pl>
2025-01-08 16:13:57 +0100

    refactor: change field name

--- src/main/java/hutnyk/notes_app/Model/DTO/UserDTO.java
+++ src/main/java/hutnyk/notes_app/Model/DTO/UserDTO.java
@@ -14,6 +14,6 @@ public class UserDTO {
     private Long id;
     private String username;
     private String email;
-    private Set<Long> notesId;
+    private Set<Long> notesSetId;
     private Set<Long> roleSetId;
 }
