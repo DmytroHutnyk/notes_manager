3a6a5ce714ba6b7ed4bd363561cac17c7f6f070f
Dmytro Hutnyk <s28785@pjwstk.edu.pl>
2025-01-08 14:23:24 +0100

    refactor: use Lombok instead of boilerplate code

--- src/main/java/hutnyk/notes_app/Model/DTO/NoteDTO.java
+++ src/main/java/hutnyk/notes_app/Model/DTO/NoteDTO.java
@@ -1,75 +1,21 @@
 package hutnyk.notes_app.Model.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 import java.util.Set;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class NoteDTO {
-
+    private Long id;
     private String title;
     private String description;
     private Date finishDate;
     private Long userId;
     private Set<Long> statusSetId;
-
-    public NoteDTO() {
-    }
-
-    public NoteDTO(String title, String description, Date finishDate,  Long userId, Set<Long> statusSetId) {
-        this.title = title;
-        this.finishDate = finishDate;
-        this.userId = userId;
-        this.statusSetId = statusSetId;
-        this.description = description;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Date getFinishDate() {
-        return finishDate;
-    }
-
-    public void setFinishDate(Date finishDate) {
-        this.finishDate = finishDate;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public Set<Long> getStatusSetId() {
-        return statusSetId;
-    }
-
-    public void setStatusSetId(Set<Long> statusSetId) {
-        this.statusSetId = statusSetId;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return "NoteDTO{" +
-                "title='" + title + '\'' +
-                "description='" + description + '\'' +
-                ", finishDate=" + finishDate +
-                ", userId=" + userId +
-                ", statusSetId=" + statusSetId +
-                '}';
-    }
 }
--- src/main/java/hutnyk/notes_app/Model/DTO/UserDTO.java
+++ src/main/java/hutnyk/notes_app/Model/DTO/UserDTO.java
@@ -1,63 +1,19 @@
 package hutnyk.notes_app.Model.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Set;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserDTO {
-
+    private Long id;
     private String username;
     private String email;
     private Set<Long> notesId;
     private Set<Long> roleSetId;
-
-    public UserDTO() {
-    }
-
-    public UserDTO(String username, String email, Set<Long> notesId, Set<Long> roleSetId) {
-        this.username = username;
-        this.email = email;
-        this.notesId = notesId;
-        this.roleSetId = roleSetId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Set<Long> getNotesId() {
-        return notesId;
-    }
-
-    public void setNotesId(Set<Long> notesId) {
-        this.notesId = notesId;
-    }
-
-    public Set<Long> getRoleSetId() {
-        return roleSetId;
-    }
-
-    public void setRoleSetId(Set<Long> roleSetId) {
-        this.roleSetId = roleSetId;
-    }
-
-    @Override
-    public String toString() {
-        return "UserDTO{" +
-                "username='" + username + '\'' +
-                ", email='" + email + '\'' +
-                ", notesId=" + notesId +
-                ", roleSetId=" + roleSetId +
-                '}';
-    }
 }
