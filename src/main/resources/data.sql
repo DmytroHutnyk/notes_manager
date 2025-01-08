1d203fb16a74a796d6b038c3cbe28f1f73ce8d17
Dmytro Hutnyk <s28785@pjwstk.edu.pl>
2025-01-08 22:40:44 +0100

    fix: removed id insertion

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
 -- Insert Roles
-INSERT INTO role (id, name) VALUES
-    (1, 'USER'),
-(2, 'PREMIUM'),
-(3, 'ADMIN');
+INSERT INTO role ( name) VALUES
+    ( 'USER'),
+('PREMIUM'),
+('ADMIN');
 
 -- Insert Users
-INSERT INTO users (id, username, password, email) VALUES
-    (1, 'user1', 'password1', 'user1@example.com'),
-(2, 'user2', 'password2', 'user2@example.com'),
-(3, 'admin', 'adminpassword', 'admin@example.com');
+INSERT INTO users (username, password, email) VALUES
+    ('user1', 'password1', 'user1@example.com'),
+('user2', 'password2', 'user2@example.com'),
+('admin', 'adminpassword', 'admin@example.com');
 
 -- Assign Roles to Users
 INSERT INTO user_role (user_id, role_id) VALUES
@@ -17,16 +17,16 @@ INSERT INTO user_role (user_id, role_id) VALUES
 (3, 3);
 
 -- Insert Statuses
-INSERT INTO status (id, name) VALUES
-    (1, 'Open'),
-(2, 'In Progress'),
-(3, 'Completed'),
-(4, 'Archived');
+INSERT INTO status (name) VALUES
+    ('Open'),
+('In Progress'),
+('Completed'),
+('Archived');
 
 -- Insert Notes
-INSERT INTO note (id, title, description, finish_date, user_id) VALUES
-(1, 'Note 1 Title', 'This is the description for Note 1', '2025-01-10', 1),
-(2, 'Note 2 Title', 'This is the description for Note 2', NULL, 2);
+INSERT INTO note (title, description, finish_date, user_id) VALUES
+('Note 1 Title', 'This is the description for Note 1', '2025-01-10', 1),
+('Note 2 Title', 'This is the description for Note 2', NULL, 2);
 
 
 -- Assign Statuses to Notes
