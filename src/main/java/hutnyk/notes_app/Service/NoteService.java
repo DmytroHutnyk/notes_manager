c0b6ecaa83ec2b4207cac80c4cb4acffc32b2e2b
Dmytro Hutnyk <s28785@pjwstk.edu.pl>
2025-01-08 17:01:26 +0100

    refactor: use specific mapper instead of general one

--- src/main/java/hutnyk/notes_app/Service/NoteService.java
+++ src/main/java/hutnyk/notes_app/Service/NoteService.java
@@ -3,7 +3,7 @@ package hutnyk.notes_app.Service;
 import hutnyk.notes_app.Model.DTO.NoteDTO;
 import hutnyk.notes_app.Model.Entity.Note;
 import hutnyk.notes_app.Repository.INoteRepository;
-import hutnyk.notes_app.Service.Mapper.EntityMapper;
+import hutnyk.notes_app.Service.Mapper.NoteMapper;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -17,30 +17,31 @@ import java.util.stream.Collectors;
 public class NoteService {
 
     private final INoteRepository noteRepository;
-    private final EntityMapper entityMapper;
+    private final NoteMapper noteMapper;
 
-    public NoteService(INoteRepository noteRepository, EntityMapper entityMapper) {
+    public NoteService(INoteRepository noteRepository, NoteMapper noteMapper) {
         this.noteRepository = noteRepository;
-        this.entityMapper = entityMapper;
+        this.noteMapper = noteMapper;
     }
 
     public List<NoteDTO> getAllNotesDTO() {
         List<Note> notes = noteRepository.findAll();
-        return notes.stream().map(entityMapper::noteToDTO).collect(Collectors.toList());
+        return notes.stream().map(noteMapper::noteToDTO).collect(Collectors.toList());
     }
-    public List<Note> getAllNotes() {
+    public List<Note> getAllNotesAdmin() {
         return noteRepository.findAll();
     }
+
     public Page<NoteDTO> getAllNotesWithPagination(int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         Page<Note> notePage = noteRepository.findAll(pageable);
-        return notePage.map(entityMapper::noteToDTO);
+        return notePage.map(noteMapper::noteToDTO);
     }
 
     public NoteDTO addNote(NoteDTO noteDTO) {
-        Note note = entityMapper.dtoToNote(noteDTO);
+        Note note = noteMapper.dtotoNote(noteDTO);
         Note savedNote = noteRepository.save(note);
-        return entityMapper.noteToDTO(savedNote);
+        return noteMapper.noteToDTO(savedNote);
     }
 
     public void deleteNoteById(Long id) {
@@ -54,13 +55,13 @@ public class NoteService {
         Note note = noteRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException("Note not found with id: " + id)
         );
-        return entityMapper.noteToDTO(note);
+        return noteMapper.noteToDTO(note);
     }
 
     public Page<NoteDTO> getNotesWithPaginationAndSorting(int page, int size, String sortBy, String sortDir) {
         Sort sort = sortDir.equalsIgnoreCase("desc") ? Sort.by(sortBy).descending() : Sort.by(sortBy).ascending();
         Pageable pageable = PageRequest.of(page, size, sort);
         Page<Note> notePage = noteRepository.findAll(pageable);
-        return notePage.map(entityMapper::noteToDTO);
+        return notePage.map(noteMapper::noteToDTO);
     }
 }
