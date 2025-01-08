66539ca47f749bb0470a3043ea095b94ddf982df
Dmytro Hutnyk <s28785@pjwstk.edu.pl>
2025-01-08 16:54:05 +0100

    refactor: use specific mapper instead of general one

--- src/main/java/hutnyk/notes_app/Service/UserService.java
+++ src/main/java/hutnyk/notes_app/Service/UserService.java
@@ -3,7 +3,7 @@ package hutnyk.notes_app.Service;
 import hutnyk.notes_app.Model.DTO.UserDTO;
 import hutnyk.notes_app.Model.Entity.User;
 import hutnyk.notes_app.Repository.IUserRepository;
-import hutnyk.notes_app.Service.Mapper.EntityMapper;
+import hutnyk.notes_app.Service.Mapper.UserMapper;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -18,16 +18,16 @@ import java.util.stream.Collectors;
 public class UserService {
 
     private final IUserRepository userRepository;
-    private final EntityMapper entityMapper;
+    private final UserMapper userMapper;
 
-    public UserService(IUserRepository userRepository, EntityMapper entityMapper) {
+    public UserService(IUserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
-        this.entityMapper = entityMapper;
+        this.userMapper = userMapper;
     }
 
     public List<UserDTO> getAllUsersDTO() {
         List<User> users = userRepository.findAll();
-        return users.stream().map(entityMapper::userToDTO).collect(Collectors.toList());
+        return users.stream().map(userMapper::userToDTO).collect(Collectors.toList());
     }
 
     public List<User> getAllUsers() {
@@ -37,13 +37,13 @@ public class UserService {
     public Page<UserDTO> getAllUsersWithPagination(int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         Page<User> userPage = userRepository.findAll(pageable);
-        return userPage.map(entityMapper::userToDTO);
+        return userPage.map(userMapper::userToDTO);
     }
 
     public UserDTO addUser(UserDTO userDTO) {
-        User user = entityMapper.dtoToUser(userDTO);
+        User user = userMapper.dtotoUser(userDTO);
         User savedUser = userRepository.save(user);
-        return entityMapper.userToDTO(savedUser);
+        return userMapper.userToDTO(savedUser);
     }
 
     public void deleteUserById(Long id) {
@@ -57,13 +57,13 @@ public class UserService {
         User user = userRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException("User not found with id: " + id)
         );
-        return entityMapper.userToDTO(user);
+        return userMapper.userToDTO(user);
     }
 
     public Page<UserDTO> getUsersWithPaginationAndSorting(int page, int size, String sortBy, String sortDir) {
         Sort sort = sortDir.equalsIgnoreCase("desc") ? Sort.by(sortBy).descending() : Sort.by(sortBy).ascending();
         Pageable pageable = PageRequest.of(page, size, sort);
         Page<User> userPage = userRepository.findAll(pageable);
-        return userPage.map(entityMapper::userToDTO);
+        return userPage.map(userMapper::userToDTO);
     }
 }
